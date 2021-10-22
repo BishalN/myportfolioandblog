7217c510a172f276db4ebfbfac19e5fa7c29a2cf
Bishal Neupane <neupanebishal07@gmail.com>
2021-10-22 12:53:36 +0545

    Hero page UI

---
+++ web/src/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  fonts: {
+    body: "'Carter One', Cursive",
+    heading: "'Carter One', Cursive",
+    mono: "'Carter One', Cursive",
+  },
+});
