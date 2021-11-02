1cc08ce9da6be040899f9dbf242df9feb4a29b0d
Bishal Neupane <neupanebishal07@gmail.com>
2021-11-02 15:04:47 +0545

    Fix base uri

--- web/src/utils/baseUrl.ts
+++ web/src/utils/baseUrl.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://bishalpb.herokuapp.com/'
+    ? 'https://bishalpb.herokuapp.com/graphql'
     : 'http://localhost:4000/graphql';
